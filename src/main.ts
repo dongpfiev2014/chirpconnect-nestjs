4c862790cd67ef870852b73809cc2ba6040e775f
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-08-14 21:59:54 +0700

    start debug in nestjs

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  console.log('Hello');
+
+  const add = (a, b) => {
+    return a + b;
+  };
+
+  const x = add(5.6, 3);
+  const y = add(4, 3);
+  const z = add(15, 3);
+  console.log('Sida');
+  console.log(x, y, z);
 }
 bootstrap();
