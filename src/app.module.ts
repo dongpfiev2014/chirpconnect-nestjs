993fb26b5c14f73559c37f64d742f4f8168471b7
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-08-25 02:01:58 +0700

    feat: Initialize database schema and seed data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mssql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize:
+          configService.get('NODE_ENV') === 'production' ? false : true,
+        logging: true,
+        options: {
+          encrypt: true,
+          enableArithAbort: true,
+          connectionTimeout: 30000,
+          requestTimeout: 30000,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  console.log('Hello');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +15,16 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api/v1', { exclude: [''] });
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const corsOrigin = configService.get<string>('CORS_ORIGIN');
+  const corsOptions = {
+    origin: corsOrigin,
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
+  const port = configService.get<number>('SERVER_PORT') || 4000;
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
