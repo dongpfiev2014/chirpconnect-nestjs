02023124e96cc46b69caee54fce5b8e47c759e3e
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-09-11 08:10:53 +0700

    build: Update build scripts for production deployment

--- src/apollo-client/apollo-client.module.ts
+++ src/apollo-client/apollo-client.module.ts
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client/core';
 import { Global, Module } from '@nestjs/common';
-import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 
 @Global()
 @Module({
@@ -13,8 +13,11 @@ import { Request } from 'express';
         const host = req.get('host');
         const apiUrl = `${protocol}://${host}/graphql`;
         return new ApolloClient({
+          link: new HttpLink({
+            uri: apiUrl,
+          }),
           cache: new InMemoryCache(),
-          uri: apiUrl,
+          // uri: apiUrl,
         });
       },
       inject: [REQUEST],
@@ -23,3 +26,69 @@ import { Request } from 'express';
   exports: ['APOLLO_CLIENT'],
 })
 export class ApolloClientModule {}
+
+//Using HTTPS with Cache
+
+// const logoutLink = onError((error) => {
+//   if (
+//     error.graphQLErrors?.length &&
+//     (error.graphQLErrors[0].extensions?.originalError as any)?.statusCode ===
+//       401
+//   ) {
+//     if (!excludedRoutes.includes(window.location.pathname)) {
+//       onLogout();
+//     }
+//   }
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: getToken(),
+//     },
+//   };
+// });
+
+// const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
+
+// const wsLink = new GraphQLWsLink(
+//   createClient({
+//     url: `${WS_URL}/graphql`,
+//     connectionParams: {
+//       token: getToken(),
+//     },
+//   }),
+// );
+
+// const splitLink = split(
+//   ({ query }) => {
+//     const definition = getMainDefinition(query);
+//     return (
+//       definition.kind === 'OperationDefinition' &&
+//       definition.operation === 'subscription'
+//     );
+//   },
+//   wsLink,
+//   httpLink,
+// );
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache({
+//     typePolicies: {
+//       Query: {
+//         fields: {
+//           chats: {
+//             keyArgs: false,
+//             merge,
+//           },
+//           messages: {
+//             keyArgs: ['chatId'],
+//             merge,
+//           },
+//         },
+//       },
+//     },
+//   }),
+//   link: logoutLink.concat(authLink).concat(splitLink),
+// });
